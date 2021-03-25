88d76185f3df173aa8331f13d05bc47e392a92be
Reynolds <obiealex@umich.edu>
2021-03-24 22:56:42 -0400

    got find letters and find charizard to work

--- diss_9.py
+++ diss_9.py
@@ -6,33 +6,56 @@ import unittest
 # Task 1: Get the URL that links to the Pokemon Charmander's webpage.
 # HINT: You will have to add https://pokemondb.net to the URL retrieved using BeautifulSoup
 def getCharmanderLink(soup):
-    pass
+    d = {}
+    base = 'https://pokemondb.net'
+    char_list = soup.find_all('a', class_= "ent-name")
+    for char in char_list:
+        d[char.text] = char.get('href',None)
+    return base+d["Charmander"]
 
 # Task 2: Get the details from the box below "Egg moves". Get all the move names and store
 #         them into a list. The function should return that list of moves.
 def getEggMoves(pokemon):
+    lst = []
     url = 'https://pokemondb.net/pokedex/'+pokemon
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    tags = soup.find_all('h3')
+    for tag in tags:
+        if tag.text == "Egg moves":
+            table = tag.find_next_sibling('div', class_= 'resp-scroll')
+
+    rows = table.find_all('a', class_= "ent-name")
+    for row in rows:
+        lst.append(row.text)
+
+    return lst
+
     #add code here
+
+    
 
 # Task 3: Create a regex expression that will find all the times that have these formats: @2pm @5 pm @10am
 # Return a list of these times without the '@' symbol. E.g. ['2pm', '5 pm', '10am']
 def findLetters(sentences):
     # initialize an empty list
-    
+    lst = []
 
     # define the regular expression
-    
+    reg = "@([0-9]+\s?[ap]m)"
 
     # loop through each sentence or phrase in sentences
-    
-
+    for sentence in sentences:
+        
     # find all the words that match the regular expression in each sentence
-       
+        words = re.findall(reg,sentence)   
 
     # loop through the found words and add the words to your empty list
-
+        for word in words:
+            lst.append(word)
 
     #return the list of the last letter of all words that begin or end with a capital letter
+    return lst
 
 
 
